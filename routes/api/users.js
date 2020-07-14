13d879e63e5aa628d312aa4dcfafff80c9162625
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-07-14 13:11:17 +0200

    +routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get()
+// @route   GET api users
+// @desc    Test route
+// @access  Public
+
+router.get('/', (req, res) => res.send('User route'));
+
+
+module.exports = router;
+
+
+
+
+// const express = require("express");
+// const router = express.Router();
+// //@route    GET api/users
+// //@desc     Test Route
+// //@access   Public
+// router.get("/", (req, res) => res.send("User Route"));
+
+// module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ connetDB();
 
 app.get('/', (req, res) => res.send('API runnig')); 
 
+// Define routs
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000; 
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
