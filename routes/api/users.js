36ec304872903b20aed1e6ddba03175c8c24834d
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-07-16 12:16:53 +0200

    jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator');
 
@@ -48,15 +50,29 @@ router.post('/', [
                 avatar,
                 password
             });
+          
             // Encrypt password
             const salt = await bcrypt.genSalt(10); 
-
             user.password = await bcrypt.hash(password, salt);
-
             await user.save();
 
+
             // Return json webtoken
-            res.send('User registered');
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+            
+            jwt.sign(
+                payload, 
+                config.get('jwtSecret'), 
+                { expiresIn: 3600 },
+                (err, token)=> {
+                    if(err) throw err;
+                    res.json({ token });
+                }
+            );
 
         } catch (err) {
             console.error(err.message);
