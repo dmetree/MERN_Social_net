6a784beef36120fb1d95b1a58606a0b0d8ec2309
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-07-29 12:24:08 +0200

    Create/Get posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api posts
-// @desc    Test route
-// @access  Public
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) => res.send('Posts route'));
+// @route   Post api/posts
+// @desc    Create a post
+// @access  Private
+
+router.post('/', [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password');
+            const newPost = Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            })
+
+            const post = await newPost.save();
+            res.json(post);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error')
+        }
+    });
+
+
+// @route   Get api/posts
+// @desc    Get posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1 }); //start with most recent. If Oldest use '1'
+        return res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+})
 
 
 module.exports = router;
