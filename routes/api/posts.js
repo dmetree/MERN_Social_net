89547e8d842449ca03441cec49ee8de474b9f198
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-07-29 12:54:21 +0200

    Get post by id

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -42,13 +42,34 @@ router.post('/', [auth, [
 // @access  Private
 router.get('/', auth, async (req, res) => {
     try {
-        const posts = await Post.find().sort({date: -1 }); //start with most recent. If Oldest use '1'
+        const posts = await Post.find().sort({ date: -1 }); //start with most recent. If Oldest use '1'
         return res.json(posts);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error')
     }
-})
+});
+
+
+// @route   Get api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        return res.json(post);
+    } catch (err) {
+        console.error(err.message);
+        if (!err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server error')
+    }
+});
 
 
 module.exports = router;
