6fa6988230f26ec2016845e9225e4b1735c6ce2d
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-08-05 11:39:50 +0200

    Login/Register

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,23 @@ import './App.css';
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () =>
   <Router>
     <Fragment>
-      <Route exact path="/" component={Navbar} />
+      <Navbar/>
+      {/* <Route exact path="/" component={Navbar} /> */}
       <Route exact path ="/" component={Landing}/>
+
+      <section className="container">
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </section>
+      
     </Fragment>
   </Router>
 
