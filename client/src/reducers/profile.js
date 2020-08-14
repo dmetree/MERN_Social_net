bcd615dcb0ff9b252548aa5d48f2d4fbd1f45783
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-08-14 11:22:47 +0200

    privateRoute

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,13 @@
+import * as a from './../actions/types';
+
+
+const initialState = {
+   
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
